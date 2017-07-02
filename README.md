eff8c532d04a4aa8f539f92c1d8fa598936d32cd
Manfred Steiner <sx@htl-kaindorf.at>
2017-07-02 10:16:55 +0200

    Init branch

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-# Angular 2+ application via Express Web-Server
+# Server logging and OOP
 
 ## Goals
 
-* How to support the Angular 2+ application from express web-server.
-* How to use a [spinner element][npm-spin] to indicate Angular application loading.
+* How to use the logger module [morgan][npm-morgan].
+* How to implement the server as object.
+* How to use config files with the module [nconf][npm-nconf]
+* How to filter [debug-sx][npm-debug-sx] messages.
 
 ## Prerequisites
 
-Install the module **spin**:
+Install the module **morgan** and **nconf**
 
 ```
 cd server
-npm install --save spin
+npm install --save morgan @types/morgan
+npm install --save nconf @types/nconf
 cd ..
 ```
 Make sure, that the Angular 2+ application bundles are available in subdirectory [ng2/dist](ng2/dist). Use the following command (or keyboard shortcut *CTRL + N* ) to build them:
@@ -24,12 +27,22 @@ cd ..
 
 ## New files
 
-* [src/views/ngmain.pug](src/views/ngmain.pug)
+* [server/src/server.ts](server/src/server.ts)
+* [server/config.json](server/config.json)
 
 ## Modified source files
 
 * [server/src/main.ts](server/src/main.ts)
 
+## Additional infos
 
+* [Node.js File System][nodejs-fs]
+* [Node.js Events][nodejs-events]
+* [Node.js Networking][nodejs-net]
 
-[npm-spin]: https://www.npmjs.com/package/spin
+[npm-morgan]: https://www.npmjs.com/package/morgan
+[npm-nconf]: https://www.npmjs.com/package/nconf
+[npm-debug-sx]: https://www.npmjs.com/package/debug-sx
+[nodejs-fs]: https://nodejs.org/api/fs.html 
+[nodejs-events]: https://nodejs.org/api/events.html
+[nodejs-net]: https://nodejs.org/api/net.html
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,71 +1,35 @@
 
 // import of additional modules (npm install ...)
-import * as express from 'express';
+import * as nconf from 'nconf';
 import * as path from 'path';
 
 // import of Node.js modules
-import * as http from 'http';
+import * as fs from 'fs';
+
+process.env['DEBUG'] = '*::*';
+import { Server } from './server';
 
 // logging with debug-sx/debug
-process.env['DEBUG'] = '*';
 import * as debugsx from 'debug-sx';
 const debug: debugsx.ISimpleLogger = debugsx.createSimpleLogger('main');
 
-// web-server
-const serverApp = express();
-serverApp.set('views', path.join(__dirname, '/views'));
-const pugEngine = serverApp.set('view engine', 'pug');
-pugEngine.locals.pretty = true;
-
-// middleware for web-server
-serverApp.use(requestHandler);
-serverApp.use(express.static(path.join(__dirname, 'public')));
-serverApp.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
-serverApp.use('/ng2', express.static(path.join(__dirname, '../../ng2/dist')));
-serverApp.get('/error', handleGetError);
-serverApp.use(error404Handler);
-serverApp.use(errorHandler);
+// configuration
+nconf.argv().env();
+const configFilename = path.join(__dirname, '../config.json');
+try {
+  fs.accessSync(configFilename, fs.constants.R_OK);
+  nconf.file(configFilename);
+} catch (err) {
+  console.log('Error on config file ' + configFilename + '\n' + err);
+  process.exit(1);
+}
 
 // start of application
-const server = http.createServer(serverApp).listen(8080);
-debug.info('Server running on port 8080');
+const server = new Server();
+server.start(8080)
+  .catch( (err) => {
+    debug.warn(err);
+  });
 
-
-// ***************************************************************************
-// Functions
-// ***************************************************************************
-
-function requestHandler (req: express.Request, res: express.Response, next: express.NextFunction) {
-  const clientSocket = req.socket.remoteAddress + ':' + req.socket.remotePort;
-  debug.info('%s %s from %s', req.method, req.url, clientSocket);
-  if (req.method === 'GET' && req.url === '/') {
-    res.render('ngmain.pug');
-  } else {
-    next();
-  }
-}
-
-function handleGetError () {
-  throw new Error('This simulates an exception....');
-}
-
-
-function error404Handler (req: express.Request, res: express.Response, next: express.NextFunction) {
-  const clientSocket = req.socket.remoteAddress + ':' + req.socket.remotePort;
-  debug.warn('Error 404 for %s %s from %s', req.method, req.url, clientSocket);
-  res.status(404).render('error404.pug');
-}
-
-
-function errorHandler (err: express.Errback, req: express.Request, res: express.Response, next: express.NextFunction) {
-  const ts = new Date().toISOString();
-  debug.warn('Error %s\n%e', ts, err);
-  res.status(500).render('error500.pug',
-    {
-      time: ts,
-      href: 'mailto:max@mustermann.com?subject=' + ts,
-      serveradmin: 'Max Mustermann'
-    });
-}
 
 
